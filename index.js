ca8e9a9ca9af314eadd114ee1b14a44bc7385285
Webcrentebot <66886048+Webcrentebot@users.noreply.github.com>
2021-06-07 21:22:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ const config = {
  * - If "example" is between the selected words, the bot will retweet any variation of the word:
  *   example,Example,EXAMPLE,ExAmPlE...
  */
-const words = 'ufrj';
+const words = 'the weeknd';
 
 /*
  * - Change 'mybot' with your bot account '@'.
  * - Example: if your account '@' is '@twitter_bot', you should write only 'twitter_bot'
  */
-const screenName = 'UFRJbot';
+const screenName = 'TheWeeknd_BOT';
 
 /*
  * If you are a not a programmer, avoid changing anything on the next lines
