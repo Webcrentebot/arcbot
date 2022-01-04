02e01724b7285bd81f08198c84c30aec0bdb88a1
Webcrentebot <66886048+Webcrentebot@users.noreply.github.com>
2022-01-04 18:36:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const Twit = require('twit');
  * - Insert each Twitter credential inside it's equivalent quote.
  */
 const config = {
-  consumer_key: 'gaPGsMOUPdIyXKRxrwkx3I7Xp',
-  consumer_secret: '0AdawaahLUStv7RPgxGgShMI4iQKDTXpbSA1ZIV2Hompyv30MC',
-  access_token: '1392587350824345600-bxGx2U75dM5gN33wg1U5rLoHQeRNup',
-  access_token_secret: 'xG1y4NTpM2Xsd2VfSUbf5Rm1fGEioXeieQd1Yiqu4Ab68',
+  consumer_key:'H7PNyhb7WY5OF2doFVtftRq4G',
+  consumer_secret: 'l42MMxUYfxg5xWAPSZhnJcjwiJmJ4cvk0w4IpYuME265WeAyCQ',
+  access_token: '1478470492470325251-r07As51xhbEtepfaNdsuZhORve5eft',
+  access_token_secret: '29pSfxhrTSWhy0R8YZnL6r3p7tL2vyCR8xSSmJ6SrgLVC',
 };
 
 /*
@@ -18,13 +18,13 @@ const config = {
  * - If "example" is between the selected words, the bot will retweet any variation of the word:
  *   example,Example,EXAMPLE,ExAmPlE...
  */
-const words = 'the weeknd';
+const words = 'Lille;
 
 /*
  * - Change 'mybot' with your bot account '@'.
  * - Example: if your account '@' is '@twitter_bot', you should write only 'twitter_bot'
  */
-const screenName = 'TheWeeknd_BOT';
+const screenName = 'Lille_BOT';
 
 /*
  * If you are a not a programmer, avoid changing anything on the next lines
